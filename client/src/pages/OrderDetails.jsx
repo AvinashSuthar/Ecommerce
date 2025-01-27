2143ef9aac066da4505a4ed2001f8928cf483199
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-01-27 14:52:40 +0530

    OrderDetails

--- client/src/pages/OrderDetails.jsx
+++ client/src/pages/OrderDetails.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { GET_SINGLE_ORDER_ROUTES } from "../utils/constants";
 import Loader from "../components/Loader";
+import { Typography } from "@mui/material";
+import "./_orderDetails.css";
 
 const OrderDetails = () => {
   const { id } = useParams();
@@ -15,10 +17,9 @@ const OrderDetails = () => {
         const res = await axios.get(`${GET_SINGLE_ORDER_ROUTES}/${id}`, {
           withCredentials: true,
         });
-        if (res.status) {
-          setOrder(res.data.order);
-          setLoading(false);
-        }
+        console.log(res);
+        setOrder(res.data.order);
+        setLoading(false);
       } catch (error) {
         setOrder(undefined);
         setLoading(false);
@@ -29,17 +30,86 @@ const OrderDetails = () => {
     getSingleOrder();
   }, []);
 
-  if (loading) {
+  if (loading || order === undefined) {
     return <Loader show={loading} />;
   }
 
   return (
     <div>
-      <div>{JSON.stringify(order)}</div>
-      <div>
-        <div className="flex">
-          <div>Order ID :</div>
-          <div>{order._id}</div>
+      <div className="orderDetailsPage">
+        <div className="orderDetailsContainer">
+          <Typography component="h1">Order #{order && order._id}</Typography>
+          <Typography>Shipping Info</Typography>
+          <div className="orderDetailsContainerBox">
+            <div>
+              <p>Name:</p>
+              <span>{order.user && order.user.name}</span>
+            </div>
+            <div>
+              <p>Phone:</p>
+              <span>{order.shippingInfo && order.shippingInfo.phoneNo}</span>
+            </div>
+            <div>
+              <p>Address:</p>
+              <span>
+                {order.shippingInfo &&
+                  `${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.state}, ${order.shippingInfo.pinCode}, ${order.shippingInfo.country}`}
+              </span>
+            </div>
+          </div>
+          <Typography>Payment</Typography>
+          <div className="orderDetailsContainerBox">
+            <div>
+              <p
+                className={
+                  order.paymentInfo && order.paymentInfo.status === "succeeded"
+                    ? "greenColor"
+                    : "redColor"
+                }
+              >
+                {order.paymentInfo && order.paymentInfo.status === "succeeded"
+                  ? "PAID"
+                  : "NOT PAID"}
+              </p>
+            </div>
+
+            <div>
+              <p>Amount:</p>
+              <span>{order.totalPrice && order.totalPrice}</span>
+            </div>
+          </div>
+
+          <Typography>Order Status</Typography>
+          <div className="orderDetailsContainerBox">
+            <div>
+              <p
+                className={
+                  order.orderStatus && order.orderStatus === "Delivered"
+                    ? "greenColor"
+                    : "redColor"
+                }
+              >
+                {order.orderStatus && order.orderStatus}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="orderDetailsCartItems">
+          <Typography>Order Items:</Typography>
+          <div className="orderDetailsCartItemsContainer">
+            {order.orderItems &&
+              order.orderItems.map((item) => (
+                <div key={item.product}>
+                  <img src={item.image} alt="Product" />
+                  <Link to={`/product/${item.product}`}>{item.name}</Link>{" "}
+                  <span>
+                    {item.quantity} X ₹{item.price} ={" "}
+                    <b>₹{item.price * item.quantity}</b>
+                  </span>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
